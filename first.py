e19bc1765130bfb373976d29d6987a8dcba9cc7e
Sino <ffarmonovsino@gmail.com>
2023-02-17 20:40:53 +0300

    commit

--- first.py
+++ first.py
@@ -1,1 +1,14 @@
 print("Hello world")
+
+print(
+    "Salom", "Kitob", "kampyuter", "stol", "gilam"
+)
+print(
+    "Salom",
+    "Kitob", 
+    "kampyuter", 
+    "stol", 
+    "gilam"
+)
+print("Salom Kitob kampyuter stol gilam")
+print("Salom " + "Kitob " + "kampyuter " + "stol " + "gilam")
