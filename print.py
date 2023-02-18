cb649460b433a740a1e3af324594c3334d1de053
Sino <ffarmonovsino@gmail.com>
2023-02-18 11:30:43 +0300

    commit

--- print.py
+++ print.py
@@ -3,3 +3,18 @@
 #masalan
 
 print("Hello world")
+
+#print funksiyasining ishlatilishi
+print(
+    "Salom", "Kitob", "kampyuter", "stol", "gilam"
+)
+print(
+    "Salom",
+    "Kitob", 
+    "kampyuter", 
+    "stol", 
+    "gilam"
+)
+print("Salom Kitob kampyuter stol gilam")
+
+print("Salom " + "Kitob " + "kampyuter " + "stol " + "gilam")
