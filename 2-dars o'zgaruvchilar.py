03bafcc2523df726d6c4dee36269654a23fe222b
Sino <ffarmonovsino@gmail.com>
2023-02-18 11:39:58 +0300

    commit

--- 2-dars o'zgaruvchilar.py
+++ 2-dars o'zgaruvchilar.py
@@ -1,11 +1,17 @@
 a = "Salom"
 print(a)
+#natija Salom
 
 b = "sarvar"
 print(a + b)
+#natija Salom sarvar
 
 print(a, "Sardor")
+#natija Salom Sardor
 
 ism = "Sarvar"
 print("assalomu alaykum", ism)
+#natija assalomu alaykum Sarvar
+
 print(ism, a)
+#natija Sarvar Salom
