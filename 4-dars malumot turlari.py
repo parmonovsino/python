570b1073e82e3c66315532b3538e82ba3421e0ad
Sino <ffarmonovsino@gmail.com>
2023-02-18 11:52:44 +0300

    commit

---
+++ 4-dars malumot turlari.py
@@ -0,0 +1,14 @@
+ism = "Ahmad"
+print(type(ism))
+#natija <class 'str'>
+
+raqam = 10
+print(type(raqam))
+#natija <class 'int'>
+
+a = 12
+print(type(a))
+#natija <class 'int'>
+
+yosh = input("yoshingizni kiriting")
+print(yosh + str(1))
