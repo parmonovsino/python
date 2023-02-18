f88d15b3fe577db28bccf6f89c16d69a9e3bd81f
Sino <ffarmonovsino@gmail.com>
2023-02-18 11:16:34 +0300

    commit

---
+++ print.py
@@ -0,0 +1,14 @@
+print("Hello world")
+
+print(
+    "Salom", "Kitob", "kampyuter", "stol", "gilam"
+)
+print(
+    "Salom",
+    "Kitob", 
+    "kampyuter", 
+    "stol", 
+    "gilam"
+)
+print("Salom Kitob kampyuter stol gilam")
+print("Salom " + "Kitob " + "kampyuter " + "stol " + "gilam")
