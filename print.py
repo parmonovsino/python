a88baf1bf0aca50fe7c8b3b619cf4e22942d62a0
Sino <ffarmonovsino@gmail.com>
2023-02-18 11:26:16 +0300

    commit

--- print.py
+++ print.py
@@ -1,4 +1,5 @@
 #biror bir so'zni ekranga chiqarish uchun biz print funksiyasidan foydalanamiz
 #print deb yozib joy tashlamasdan qavs ochamiz qavsni ichidan qo'shtirnoq ochib qo'shtirnoqlarni ichiga xoxlagan so'zimizni yozamiz
 #masalan
+
 print("Hello world")
