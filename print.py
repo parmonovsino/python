4b66e2df072184f67721beb0acdf10c43924e758
Sino <ffarmonovsino@gmail.com>
2023-02-18 11:25:57 +0300

    commit

--- print.py
+++ print.py
@@ -1,16 +1,4 @@
 #biror bir so'zni ekranga chiqarish uchun biz print funksiyasidan foydalanamiz
-
+#print deb yozib joy tashlamasdan qavs ochamiz qavsni ichidan qo'shtirnoq ochib qo'shtirnoqlarni ichiga xoxlagan so'zimizni yozamiz
+#masalan
 print("Hello world")
-
-print(
-    "Salom", "Kitob", "kampyuter", "stol", "gilam"
-)
-print(
-    "Salom",
-    "Kitob", 
-    "kampyuter", 
-    "stol", 
-    "gilam"
-)
-print("Salom Kitob kampyuter stol gilam")
-print("Salom " + "Kitob " + "kampyuter " + "stol " + "gilam")
